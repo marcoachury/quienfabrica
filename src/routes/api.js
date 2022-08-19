9e17d2fd6776eb6ccfa4f23d79448e4f4ba9acee
ZUR1C4T0 <ivanulloque19@gmail.com>
2022-08-19 14:20:40 -0500

    estructura de carpeta

--- src/routes/api.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-const router = Router();
-
-router.get("/business", (req, res) => {
-  res.json({ message: "servidors funcionando" });
-});
-
-export default router;
